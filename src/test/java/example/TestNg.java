a87ac82917d6a094f8b18d0fdeb6fd25b548907f
YourBoB <zhaojunbao123@163.com>
2017-08-14 13:34:04 +1200

    commit

---
+++ src/test/java/example/TestNg.java
@@ -0,0 +1,39 @@
+package example;
+
+ import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;		
+public class TestNg {		
+	    private WebDriver driver;
+	    
+		@Test				
+		public void testEasy() {	
+	//		driver.get("http://google.com");  
+		//	String title = driver.getTitle();				 
+		//	AssertJUnit.assertTrue(title.contains("Google")); 
+			AssertJUnit.assertEquals(true, true);
+		}	
+		@Test				
+		public void testFail() {	
+	//		driver.get("http://google.com");  
+		//	String title = driver.getTitle();				 
+		//	AssertJUnit.assertTrue(title.contains("Google")); 
+			System.out.println("test...");
+			AssertJUnit.assertEquals(true, false);
+			
+			
+			
+			
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		  //  driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			//driver.quit();			
+		}		
+}
